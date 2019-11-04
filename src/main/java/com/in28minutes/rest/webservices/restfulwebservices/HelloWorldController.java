1d14f14a15310eb9d519e5e3ffde7395aa4293c6
moonesh <moonesh@havnetch.in>
2019-11-04 14:24:36 +0530

    Version 6 of the microservice

--- src/main/java/com/in28minutes/rest/webservices/restfulwebservices/HelloWorldController.java
+++ src/main/java/com/in28minutes/rest/webservices/restfulwebservices/HelloWorldController.java
@@ -9,12 +9,12 @@ public class HelloWorldController {
 
 	@GetMapping(path = "/hello-world")
 	public String helloWorld() {
-		return "Hello World V3";
+		return "Hello World V6";
 	}
 
 	@GetMapping(path = "/hello-world-bean")
 	public HelloWorldBean helloWorldBean() {
-		return new HelloWorldBean("Hello World - Changed - V3");
+		return new HelloWorldBean("Hello World - Changed - V6");
 	}
 	
 	@GetMapping(path = "/hello-world/path-variable/{name}")
