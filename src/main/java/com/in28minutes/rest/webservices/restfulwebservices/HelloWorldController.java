f4a60fa40b93b32ce8a1ff472534e5a8f7d1c9ec
moonesh <mooneshkachroo@hotmail.com>
2019-12-24 19:11:41 +0530

    Changed Version from 10 to 2

--- src/main/java/com/in28minutes/rest/webservices/restfulwebservices/HelloWorldController.java
+++ src/main/java/com/in28minutes/rest/webservices/restfulwebservices/HelloWorldController.java
@@ -9,7 +9,7 @@ public class HelloWorldController {
 
 	@GetMapping(path = "/hello-world")
 	public String helloWorld() {
-		return "Hello World Version 10 ..... ";
+		return "Hello World Version 2 ..... ";
 	}
 
 	@GetMapping(path = "/hello-world-bean")
